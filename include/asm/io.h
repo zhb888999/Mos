ca40c96dc67e1a92d895e5e81d3be2dd3a5c1328
zhb888999 <zhb888999@126.com>
2017-11-04 03:25:14 -0700

    最新

--- include/asm/io.h
+++ include/asm/io.h
@@ -1,87 +1,32 @@
 #ifndef _IO_H
 #define _IO_H
-#define hlt() 								\
-({  										\
-	asm volatile("hlt":::); 				\
+
+#define outb(value,port) asm volatile("outb %%al,%%dx\n\t" ::"al"(value),"dx"(port))
+#define outw(value,port) asm volatile("outw %%ax,%%dx\n\t" ::"ax"(value),"dx"(port))
+#define outl(value,port) asm volatile("outl %%eax,%%dx\n\t" ::"eax"(value),"dx"(port))
+#define inb(port) \
+({	\
+	unsigned char v; \
+	asm volatile ("inb %%dx,%%al" \
+					:"=a" (v):"dx" (port) \
+					:); \
+	v; \
 })
-#define cli() 								\
-({  										\
-	asm volatile("cli":::); 				\
+#define inw(port) \
+({	\
+	unsigned short int v; \
+	asm volatile ("inw %%dx,%%ax" \
+					:"=a" (v):"dx" (port) \
+					:); \
+	v; \
 })
-#define sti() 								\
-({  										\
-	asm volatile("sti":::); 				\
+#define inl(port) \
+({	\
+unsigned int v; \
+	asm volatile ("inl %%dx,%%eax" \
+					:"=a" (v):"dx" (port) \
+					:); \
+v; \
 })
-#define outb(value,port) 					\
-({											\
-	asm volatile("outb %%al,%%dx\n\t" 		\
-					: 						\
-					:"al"(value),"dx"(port) \
-					:						\
-				); 							\
-})
-#define outw(value,port) 					\
-({											\
-	asm volatile("outw %%ax,%%dx\n\t" 		\
-					: 						\
-					:"ax"(value),"dx"(port) \
-					: 						\
-				);							\
-})
-#define outl(value,port) 					\
-({											\
-	asm volatile("outl %%eax,%%dx\n\t" 		\
-					: 						\
-					:"eax"(value),"dx"(port) \
-					: 						\
-					);						\
-})
-#define inb(port) 							\
-({  										\
-	unsigned char v; 						\
-	asm volatile ("inb %%dx,%%al" 			\
-					:"=a" (v) 				\
-					:"dx" (port) 			\
-					: 						\
-				); 							\
-	v; 										\
-})
-#define inw(port)						 	\
-({ 											\
-	unsigned short int v; 					\
-	asm volatile ("inw %%dx,%%ax" 			\
-					:"=a" (v) 				\
-					:"dx" (port) 			\
-					: 						\
-				); 							\
-	v;										\
-})
-#define inl(port) 							\
-({ 											\
-unsigned int v; 							\
-	asm volatile ("inl %%dx,%%eax" 			\
-					:"=a" (v) 				\
-					:"dx" (port) 			\
-					: 						\
-				); 							\
-	v; 										\
-})
-#define store_eflags(eflags) 				\
-({											\
-	asm volatile("pushf\n\t" 				\
-				 "pop	%%eax\n\t" 			\
-					:"=a" (eflags) 			\
-					: 						\
-					: 						\
-					); 						\
-})
-#define load_eflags(eflags)					\
-({											\
-	asm volatile("push	%%eax\n\t" 			\
-				 "popf\n\t"					\
-				 : 							\
-				 :"a"(eflags) 				\
-				 : 							\
-				 ); 						\
-})
+
 #endif
--- init/Main.c
+++ init/Main.c
@@ -1,4 +1,5 @@
-#include<Mos/kernel.h>
+#include <Mos/kernel.h>
+#include <asm/system.h>
 //system模块最大32768行
 extern char *_pg_dir;
 extern char *pg0;
@@ -16,10 +17,13 @@ unsigned int mem_size; //以Mb为单位
 
 void Main(void)
 {
-
 	unsigned short int *mem_low_szie = (int *)(0x90000);
 	unsigned short int *mem_hi_szie  = (int *)(0x90002);
 	mem_size = *mem_low_szie/1024 + *mem_hi_szie/16 + 1;
+	unsigned short int latch = 0x2e9a;
+	outb(0x36,0x43);
+	outb(0x9a,0x40);
+	outb(0x2e,0x40);
 	printk("pg_dir:%p\n",&_pg_dir);
 	printk("pg0:%p\n",&pg0);
 	printk("pg1:%p\n",&pg1);
@@ -29,6 +33,7 @@ void Main(void)
 	printk("idt:%p\n",&_idt);
 	printk("init_stack:%p\n",&init_stack);
 	printk("mem_size:%d\n",mem_size);
+	asm("int $0x18"::);
 	sti();
 	for(;;)
 		hlt();
